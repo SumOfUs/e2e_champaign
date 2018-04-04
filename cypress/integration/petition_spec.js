61daa946336bf56f639ec314772b1cb0632f6113
Omar Sahyoun <osahyoun@gmail.com>
2018-04-04 13:52:42 +0100

    Pass environment variables with command, and use axios for ak request

--- cypress/integration/petition_spec.js
+++ cypress/integration/petition_spec.js
@@ -1,13 +1,19 @@
 
-const {
-  AK_USERNAME,
-  AK_PASSWORD,
-} = process.env;
-
-const host = `https://${AK_USERNAME}:${AK_PASSWORD}@act.sumofus.org/rest/v1`;
+const host = `https://${Cypress.env('AK_USERNAME')}:${Cypress.env('AK_PASSWORD')}@act.sumofus.org/rest/v1`;
 const axios = require('axios');
-
 let email;
+
+const getUserFromActionKit = () => {
+  return new Cypress.Promise((resolve, reject) => {
+    return axios
+      .get(`${host}/user/?email=${encodeURIComponent(email)}`)
+      .then( resp => resolve(resp) )
+      .catch( err => {
+        console.log("ActionKit Request Error", err);
+        return reject(err);
+      })
+  });
+}
 
 describe("Petition Action", () => {
   before( () => {
@@ -16,19 +22,21 @@ describe("Petition Action", () => {
   })
 
   it("is successfully submitted and action is successfully written to actionkit", () => {
-    let reqCount = 9;
+    let reqCount = 0;
+
     const req = () => {
-      cy
-      .request(`${host}/user/?email=${encodeURIComponent(email)}`)
-      .then((resp) => {
-        if (resp.status === 200 && resp.body.objects.length > 0) {
-          expect(resp.body.objects[0].first_name).to.eq('Omar');
-        } else {
-          if(reqCount > 10) new Error("Too many requests!");
-          reqCount += 1;
-          req();
-        }
-      });
+      cy.wrap(null).then(() => {
+        return getUserFromActionKit()
+          .then( resp => {
+            if (resp.status === 200 && resp.data.objects.length > 0) {
+              expect(resp.data.objects[0].first_name).to.eq('Omar');
+            } else {
+              if(reqCount > 7) new Error("Too many requests!");
+              reqCount += 1;
+              req();
+            }
+          })
+        });
     };
 
     cy.visit("https://actions.sumofus.org/a/stop-credit-card-giants-from-funding-facist-hatred-and-violence")
