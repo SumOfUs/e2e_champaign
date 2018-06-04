bca6386513dc7b98daa794ca57c7b76218ad80c0
Omar Sahyoun <osahyoun@gmail.com>
2018-06-04 17:33:29 +0100

    Unsubscribe user after running test

--- cypress/integration/petition_spec.js
+++ cypress/integration/petition_spec.js
@@ -13,6 +13,18 @@ const getUserFromActionKit = () => {
         return reject(err);
       })
   });
+};
+
+const unsubscribeUserFromActionKit = () => {
+  return new Cypress.Promise((resolve, reject) => {
+    return axios
+      .post(`${host}/action/`, {email: email, page: 'unsubscribe'})
+      .then( resp => resolve(resp) )
+      .catch( err => {
+        console.log("ActionKit Request Error", err);
+        return reject(err);
+      })
+  });
 }
 
 describe("Petition Action", () => {
@@ -31,13 +43,22 @@ describe("Petition Action", () => {
             if (resp.status === 200 && resp.data.objects.length > 0) {
               expect(resp.data.objects[0].first_name).to.eq('Omar');
             } else {
-              if(reqCount > 7) new Error("Too many requests!");
+              if(reqCount > 5) {
+                throw new Error(`User wasn't found on ActionKit. We checked ${reqCount} times!`);
+              }
               reqCount += 1;
               req();
             }
           })
         });
     };
+
+    const unsubscribe = () => {
+      cy.wrap(null).then(() => {
+        return unsubscribeUserFromActionKit()
+          .then( resp => cy.log(resp))
+      });
+    }
 
     cy.visit("https://actions.sumofus.org/a/stop-credit-card-giants-from-funding-facist-hatred-and-violence")
 
@@ -53,11 +74,13 @@ describe("Petition Action", () => {
       cy.get(".selectize-input")
         .click()
         .get(".selectize-input input")
-        .type("United Kingdom {enter}")
+        .type("{backspace}Argentina {enter}")
 
       cy.contains("I'm a Mastercard customer").click();
 
-      cy.get("button").click().then(req);
+      cy.get("button").click()
+        .then(req)
+        .then(unsubscribe)
     });
 
     cy.contains("Will you make a donation?");
