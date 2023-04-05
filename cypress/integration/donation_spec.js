9979351d8bb3cccd17ca0dc5de7c4ed8265e59ed
Subbiah <49471498+subbiahsn@users.noreply.github.com>
2023-04-05 16:05:20 +0530

    Updates URL

--- cypress/integration/donation_spec.js
+++ cypress/integration/donation_spec.js
@@ -4,99 +4,99 @@ const EMAIL = "omar+cc_test@sumofus.org";
 
 const gateway = braintree.connect({
   environment: braintree.Environment.Production,
-  merchantId: Cypress.env('MERCHANT_ID'),
-  publicKey: Cypress.env('PUBLIC_KEY'),
-  privateKey: Cypress.env('PRVIATE_KEY'),
+  merchantId: Cypress.env("MERCHANT_ID"),
+  publicKey: Cypress.env("PUBLIC_KEY"),
+  privateKey: Cypress.env("PRVIATE_KEY"),
 });
 
 const getTransactions = () => {
   const MS_PER_MINUTE = 60000;
-  const fiveMinutesAgo = new Date((new Date()) - 1 * MS_PER_MINUTE)
-    .toISOString();
+  const fiveMinutesAgo = new Date(new Date() - 1 * MS_PER_MINUTE).toISOString();
 
   return new Cypress.Promise((resolve, reject) => {
+    gateway.transaction.search(
+      function (search) {
+        search.customerEmail().is(EMAIL);
+        search.amount().is("2.00");
+        search.createdAt().min(fiveMinutesAgo);
+      },
+      function (err, response) {
+        console.log("RESPONSE");
+        response.each((err, transaction) => {
+          console.log("see", transaction);
+        });
 
-    gateway.transaction.search(function (search) {
-      search.customerEmail().is(EMAIL);
-      search.amount().is('2.00');
-      search.createdAt().min(fiveMinutesAgo)
-    }, function (err, response) {
-      console.log("RESPONSE");
-      response.each( (err, transaction) => {
-        console.log("see", transaction);
-      });
-
-      resolve(response);
-    });
-  })
+        resolve(response);
+      }
+    );
+  });
 };
 
 describe("first test", () => {
   it("does stuff", () => {
-    cy.visit("https://actions.sumofus.org/a/donate");
-    cy.get('a.AmountSelection__currency-toggle').click();
-    cy.get('select.AmountSelection__currency-selector')
-      .select('GBP')
+    cy.visit("https://actions.eko.org/a/donate");
+    cy.get("a.AmountSelection__currency-toggle").click();
+    cy.get("select.AmountSelection__currency-selector").select("GBP");
 
     cy.contains("£2").click();
 
     cy.get("input[name='name']")
       .type("Glen Berman")
-      .should('have.value', 'Glen Berman');
+      .should("have.value", "Glen Berman");
 
-    cy.get("input[name='email']")
-      .type(EMAIL)
-      .should('have.value', EMAIL);
+    cy.get("input[name='email']").type(EMAIL).should("have.value", EMAIL);
 
     cy.get(".SweetSelect.sweet-placeholder")
       .click()
       .get(".SweetSelect.sweet-placeholder input")
-      .type("United States {enter}")
+      .type("United States {enter}");
 
     cy.contains("Proceed to payment").click();
 
-    cy.get(".Button-root.DonateButton")
+    cy.get(".Button-root.DonateButton");
 
-    cy.contains("Securely store my payment information").click()
+    cy.contains("Securely store my payment information").click();
 
-    cy.get('iframe#braintree-hosted-field-number')
-      .then( iframe => {
-        const doc = iframe.contents().find("body");
+    cy.get("iframe#braintree-hosted-field-number").then((iframe) => {
+      const doc = iframe.contents().find("body");
 
-        cy.wrap(doc).find('input#credit-card-number')
-            .type(Cypress.env('CC_NUM'))
+      cy.wrap(doc).find("input#credit-card-number").type(Cypress.env("CC_NUM"));
     });
 
-    cy.get('iframe#braintree-hosted-field-cvv')
-      .then( iframe => {
-        const doc = iframe.contents().find("body");
+    cy.get("iframe#braintree-hosted-field-cvv").then((iframe) => {
+      const doc = iframe.contents().find("body");
 
-        cy.wrap(doc).find("input[name='cvv']")
-            .type(Cypress.env('CC_CVV'), {force: true});
+      cy.wrap(doc)
+        .find("input[name='cvv']")
+        .type(Cypress.env("CC_CVV"), { force: true });
     });
 
-    cy.get('iframe#braintree-hosted-field-expirationDate')
-      .then( iframe => {
-        const doc = iframe.contents().find("body");
+    cy.get("iframe#braintree-hosted-field-expirationDate").then((iframe) => {
+      const doc = iframe.contents().find("body");
 
-        cy.wrap(doc).find("input[name='expiration']")
-          .type(Cypress.env('CC_EXP'), {force: true});
+      cy.wrap(doc)
+        .find("input[name='expiration']")
+        .type(Cypress.env("CC_EXP"), { force: true });
     });
 
     cy.get(".Button-root.DonateButton").click();
 
     cy.contains("You're awesome");
-  })
+  });
 
-  it('has recorded the transaction', () => {
+  it("has recorded the transaction", () => {
     cy.wrap(null).then(() => {
       return getTransactions().then((response) => {
-        assert.isAbove(response.ids.length, 0, "no braintree transactions returned");
+        assert.isAbove(
+          response.ids.length,
+          0,
+          "no braintree transactions returned"
+        );
         response.each(function (err, transaction) {
           cy.log(transaction.status);
-          expect(transaction.status).to.eq('submitted_for_settlement')
+          expect(transaction.status).to.eq("submitted_for_settlement");
         });
       });
     });
   });
-})
+});
--- cypress/integration/petition_spec.js
+++ cypress/integration/petition_spec.js
@@ -1,86 +1,85 @@
-
-const host = `https://${Cypress.env('AK_USERNAME')}:${Cypress.env('AK_PASSWORD')}@act.sumofus.org/rest/v1`;
-const axios = require('axios');
+const host = `https://${Cypress.env("AK_USERNAME")}:${Cypress.env(
+  "AK_PASSWORD"
+)}@act.sumofus.org/rest/v1`;
+const axios = require("axios");
 let email;
 
 const getUserFromActionKit = () => {
   return new Cypress.Promise((resolve, reject) => {
     return axios
       .get(`${host}/user/?email=${encodeURIComponent(email)}`)
-      .then( resp => resolve(resp) )
-      .catch( err => {
+      .then((resp) => resolve(resp))
+      .catch((err) => {
         console.log("ActionKit Request Error", err);
         return reject(err);
-      })
+      });
   });
 };
 
 const unsubscribeUserFromActionKit = () => {
   return new Cypress.Promise((resolve, reject) => {
     return axios
-      .post(`${host}/action/`, {email: email, page: 'unsubscribe'})
-      .then( resp => resolve(resp) )
-      .catch( err => {
+      .post(`${host}/action/`, { email: email, page: "unsubscribe" })
+      .then((resp) => resolve(resp))
+      .catch((err) => {
         console.log("ActionKit Request Error", err);
         return reject(err);
-      })
+      });
   });
-}
+};
 
 describe("Petition Action", () => {
-  before( () => {
-    const timestamp = +(new Date());
+  before(() => {
+    const timestamp = +new Date();
     email = `omar+live-test-${timestamp}@sumofus.org`;
-  })
+  });
 
   it("is successfully submitted and action is successfully written to actionkit", () => {
     let reqCount = 0;
 
     const req = () => {
       cy.wrap(null).then(() => {
-        return getUserFromActionKit()
-          .then( resp => {
-            if (resp.status === 200 && resp.data.objects.length > 0) {
-              expect(resp.data.objects[0].first_name).to.eq('Omar');
-            } else {
-              if(reqCount > 5) {
-                throw new Error(`User wasn't found on ActionKit. We checked ${reqCount} times!`);
-              }
-              reqCount += 1;
-              req();
+        return getUserFromActionKit().then((resp) => {
+          if (resp.status === 200 && resp.data.objects.length > 0) {
+            expect(resp.data.objects[0].first_name).to.eq("Omar");
+          } else {
+            if (reqCount > 5) {
+              throw new Error(
+                `User wasn't found on ActionKit. We checked ${reqCount} times!`
+              );
             }
-          })
+            reqCount += 1;
+            req();
+          }
         });
+      });
     };
 
     const unsubscribe = () => {
       cy.wrap(null).then(() => {
-        return unsubscribeUserFromActionKit()
-          .then( resp => cy.log(resp))
+        return unsubscribeUserFromActionKit().then((resp) => cy.log(resp));
       });
-    }
+    };
 
-    cy.visit("https://actions.sumofus.org/a/stop-credit-card-giants-from-funding-facist-hatred-and-violence")
+    cy.visit(
+      "https://actions.eko.org/a/stop-credit-card-giants-from-funding-facist-hatred-and-violence"
+    );
 
-    cy.get('.petition-bar__main form').within(($form) => {
-      cy.get("input[type='email']")
-        .type(email)
-        .should('have.value', email);
+    cy.get(".petition-bar__main form").within(($form) => {
+      cy.get("input[type='email']").type(email).should("have.value", email);
 
       cy.get("input[name='name']")
         .type("Omar Sahyoun")
-        .should('have.value', 'Omar Sahyoun');
+        .should("have.value", "Omar Sahyoun");
 
       cy.get(".selectize-input")
         .click()
         .get(".selectize-input input")
-        .type("{backspace}Argentina {enter}")
+        .type("{backspace}Argentina {enter}");
 
       cy.contains("I'm a Mastercard customer").click();
 
-      cy.get("button").click()
-        .then(req)
-        .then(unsubscribe)
+      cy.get("button").click().then(req).then(unsubscribe);
     });
 
     cy.contains("Can you chip in");
