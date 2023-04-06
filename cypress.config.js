962550e207eda5d04f8a83399d3d306f531d5d28
Subbiah <49471498+subbiahsn@users.noreply.github.com>
2023-04-06 17:11:39 +0530

    Updates packages

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 9000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+}
